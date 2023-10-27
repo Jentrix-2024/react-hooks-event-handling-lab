0237fbbffd182d82a8bba0329c3b91de72aa65aa
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-10-27 23:07:44 +0300

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from 'react';
+
+function Keypad() {
+  const handlePasswordChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input type="password" onChange={handlePasswordChange} />
+    </div>
+  );
 }
 
 export default Keypad;
